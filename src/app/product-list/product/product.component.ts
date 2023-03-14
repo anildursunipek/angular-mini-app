622efaac47c6ae4d7b9181842e7563682de0f48a
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-03-14 20:55:38 +0300

    Add static data to product component

--- src/app/product-list/product/product.component.ts
+++ src/app/product-list/product/product.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  // Fields and methods
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  product = {
+    id: 1,
+    name: "Iphone 14",
+    price: 1000,
+    imageUrl: "iphone_14_.jpg",
+    isActive: true
   }
 
 }
