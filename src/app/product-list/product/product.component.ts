de9e5dede1171e33294421c65cb6015441c0a88b
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-03-15 02:15:30 +0300

    Add productList template

--- src/app/product-list/product/product.component.ts
+++ src/app/product-list/product/product.component.ts
@@ -12,12 +12,39 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  product = {
-    id: 1,
-    name: "Iphone 14",
-    price: 1000,
-    imageUrl: "iphone_14_.jpg",
-    isActive: true
-  }
+  // product = {
+  //   id: 1,
+  //   name: "Iphone 14",
+  //   price: 1000,
+  //   imageUrl: "iphone_14_.jpg",
+  //   isActive: true
+  // }
 
+  private products:any[] = [
+    {
+      id: 1,
+      name: "Iphone 14",
+      price: 1000,
+      imageUrl: "iphone_14_.jpg",
+      isActive: true
+    },
+    {
+      id: 2,
+      name: "Iphone 15",
+      price: 1100,
+      imageUrl: "iphone_14_.jpg",
+      isActive: false
+    },
+    {
+      id: 3,
+      name: "Iphone 16",
+      price: 1200,
+      imageUrl: "iphone_14_.jpg",
+      isActive: false
+    }
+  ]
+
+  getProducts(){
+    return this.products.filter(p => p.isActive)
+  }
 }
