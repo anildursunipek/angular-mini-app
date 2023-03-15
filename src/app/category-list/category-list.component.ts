4c9012e96797226f60ac5cd8d764ac41afbd0a4f
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-03-15 14:07:04 +0300

    Add selectCategory and unSelectCategory method to category-list component

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -10,6 +10,7 @@ import { CategoryRepository } from '../models/category.repository';
 export class CategoryListComponent implements OnInit {
   categories: Category[];
   categoryRepository: CategoryRepository;
+  selectedCategory: Category | null;
 
   constructor() {
     this.categoryRepository = new CategoryRepository();
@@ -19,4 +20,11 @@ export class CategoryListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectCategory(category: Category){
+    if(this.selectedCategory == category){
+      this.selectedCategory = null;
+      return;
+    }
+    this.selectedCategory = category;
+  }
 }
