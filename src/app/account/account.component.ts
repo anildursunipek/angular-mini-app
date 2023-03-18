11f60976f93c33cc2dc6c0a4de0846ad4ba4964f
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-03-18 18:52:16 +0300

    Add create account form validations

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'account',
@@ -7,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
   isLoginMode:boolean = true;
+  model:any = {
+
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleAuth(form:NgForm){
+    if(!form.valid){
+      console.log("Form is invalid");
+      return;
+    }
+    console.log("Form is valid");
+  }
   createAccount(){
     console.log("Create Account Works....");
     return;
